54ea44479fbdc4372e35848246849450458ceec8
Marwan Azuz <marwanazuz+cinnabyte@gmail.com>
2025-02-20 17:47:33 +0100

    Fix typo

--- .config/nvim/lua/keymaps/general.lua
+++ .config/nvim/lua/keymaps/general.lua
@@ -1,4 +1,7 @@
 vim.keymap.set("n", "<Tab>", "<cmd>WhichKey<CR>", { desc = "Call yourself…" })
 
 vim.keymap.set({ "n", "v" }, "p", '"0p', { noremap = true })
-vim.keymap.set({ "n", "v" }, "p", '"0P', { noremap = true })
+vim.keymap.set({ "n", "v" }, "P", '"0P', { noremap = true })
+
+vim.keymap.set("n", "dd", '"_dd', { noremap = true })
+vim.keymap.set("v", "d", '"_d', { noremap = true })
