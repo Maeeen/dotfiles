f9274d89f2664e551f99bf840393627f243be407
Marwan Azuz <marwan.azuz@aurorasgrid.com>
2026-03-31 10:31:04 +0200

    dsq

--- .config/nvim/lua/keymaps/general.lua
+++ .config/nvim/lua/keymaps/general.lua
@@ -1,1 +1,4 @@
 vim.keymap.set("n", "<Tab>", "<cmd>WhichKey<CR>", { desc = "Call yourself…" })
+vim.keymap.set("n", "<leader>cL", function()
+  vim.fn.setreg("+", vim.fn.expand("%:p") .. ":" .. vim.fn.line("."))
+end, { desc = "Copy file path with line number" })
